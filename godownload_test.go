6a2487200c0d126c794e706ac5e865fbaf3aa336
Sergey Romanov <xxsmotur@gmail.com>
2015-07-28 23:24:48 +0500

    Add Download test

--- godownload_test.go
+++ godownload_test.go
@@ -4,12 +4,26 @@ import
 (
 	"testing"
 	"os"
+	"fmt"
 )
 
-func Remove(){
-
+func exist(path string) bool {
+	return checkExist(path)
 }
 
-func TestDownload(t *testing.T){
+func remove(t *testing.T, path string){
+	err := os.Remove(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
 
+
+func TestDownload(t *testing.T){
+	Download("https://github.com/saromanov/godownload/archive/master.zip", nil)
+	path := "master.zip"
+	if !exist(path) {
+		t.Fatal(fmt.Sprintf("Downloaded file %s", path))
+	}
+	remove(t, path)
 }
