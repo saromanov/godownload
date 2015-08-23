f95e4a875137dfebc4966fb71116657c365e081f
Sergey Romanov <xxsmotur@gmail.com>
2015-08-23 23:10:07 +0500

    Update tests

--- godownload_test.go
+++ godownload_test.go
@@ -1,11 +1,10 @@
 package godownload
 
-import
-(
-	"testing"
-	"os"
+import (
 	"fmt"
 	"io/ioutil"
+	"os"
+	"testing"
 )
 
 //http://www.gnu.org/software/wget/manual/wget.html
@@ -14,14 +13,14 @@ func exist(path string) bool {
 	return checkExist(path)
 }
 
-func remove(t *testing.T, path string){
+func remove(t *testing.T, path string) {
 	err := os.Remove(path)
 	if err != nil {
 		t.Fatal(err)
 	}
 }
 
-func createFileWithLinks(filename string){
+func createFileWithLinks(filename string) {
 	data := `This file contains interesting links!
 	First: https://github.com/saromanov/godownload/archive/master.zip \n
 	Second: http://arxiv.org/pdf/1206.5538v3.pdf`
@@ -32,10 +31,9 @@ func createFileWithLinks(filename string){
 	}
 }
 
-
-
-func TestDownload(t *testing.T){
-	Download("https://github.com/saromanov/godownload/archive/master.zip", nil)
+func TestDownload(t *testing.T) {
+	gd := &GoDownload{}
+	gd.Download("https://github.com/saromanov/godownload/archive/master.zip", nil)
 	path := "master.zip"
 	if !exist(path) {
 		t.Fatal(fmt.Sprintf("Downloaded file %s not found", path))
@@ -44,8 +42,9 @@ func TestDownload(t *testing.T){
 }
 
 func TestDownloadAlwaysNew(t *testing.T) {
-	Download("https://github.com/saromanov/godownload/archive/master.zip", nil)
-	Download("https://github.com/saromanov/godownload/archive/master.zip", &Options{
+	gd := &GoDownload{}
+	gd.Download("https://github.com/saromanov/godownload/archive/master.zip", nil)
+	gd.Download("https://github.com/saromanov/godownload/archive/master.zip", &Options{
 		Outpath: "master.zip", Alwaysnew: true})
 	if !exist("master_2.zip") {
 		t.Fatal(fmt.Sprintf("Downloaded file %s not found", "master_2.zip"))
@@ -54,15 +53,16 @@ func TestDownloadAlwaysNew(t *testing.T) {
 	remove(t, "master_2.zip")
 }
 
-func TestDownloadMany (t *testing. T) {
+func TestDownloadMany(t *testing.T) {
 	path1 := "first.zip"
 	path2 := "second.zip"
-	items := []*Options{ 
-		&Options{Url: "https://github.com/saromanov/godownload/archive/master.zip", Outpath: "first.zip"}, 
+	items := []*Options{
+		&Options{Url: "https://github.com/saromanov/godownload/archive/master.zip", Outpath: "first.zip"},
 		&Options{Url: "http://arxiv.org/pdf/1206.5538v3.pdf", Outpath: "second.zip"},
 	}
 
-	DownloadMany(items)
+	gd := &GoDownload{}
+	gd.DownloadMany(items)
 	if !exist(path1) {
 		t.Fatal(fmt.Sprintf("Downloaded file %s not found", path1))
 	}
@@ -79,7 +79,9 @@ func TestFromFile(t *testing.T) {
 	path1 := "master.zip"
 	path2 := "1206.5538v3.pdf"
 	createFileWithLinks("simple")
-	FromFile("simple")
+
+	gd := &GoDownload{}
+	gd.FromFile("simple")
 	if !exist(path1) {
 		t.Fatal(fmt.Sprintf("Downloaded file %s not found", path1))
 	}
