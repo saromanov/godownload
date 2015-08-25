7277597c72c6f6ee2ed98e396fb9ae43b8f3c97f
Sergey Romanov <xxsmotur@gmail.com>
2015-08-25 23:24:21 +0500

    Update fileCount

--- utils.go
+++ utils.go
@@ -1,10 +1,9 @@
 package godownload
 
-import
-(
-	"path/filepath"
-	"io/ioutil"
+import (
 	"fmt"
+	"io/ioutil"
+	"path/filepath"
 	//"strings"
 )
 
@@ -15,7 +14,7 @@ import
 //
 //TODO: Resolve bug with not names reaching out of order (file_1, file_5)
 //After this, new file will create with name file_3, but more correct is file_6
-func fileCount(path string)int {
+func fileCount(path string) int {
 	count := 0
 	dir := filepath.Dir(path)
 	dirdata, err := ioutil.ReadDir(dir)
@@ -24,9 +23,10 @@ func fileCount(path string)int {
 	}
 	currentname := filepath.Base(path)
 	ext := filepath.Ext(currentname)
+	currentname = currentname[0:len(currentname)-len(ext)]
 	pattern := fmt.Sprintf("%s_*", currentname)
 	if ext != "" {
-		pattern = fmt.Sprintf("%s_*.%s", currentname, ext)
+		pattern = fmt.Sprintf("%s_*%s", currentname, ext)
 	}
 
 	for _, item := range dirdata {
@@ -35,5 +35,5 @@ func fileCount(path string)int {
 			count += 1
 		}
 	}
-	return count+1
+	return count + 1
 }
